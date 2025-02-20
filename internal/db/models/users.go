9aa853a402dfc1f489bf28bb541e1ff5d80e9656
mojtaba-esk <mojtaba@celestia.org>
2025-02-20 11:45:20 +0100

    update user fields

--- internal/db/models/users.go
+++ internal/db/models/users.go
@@ -1,11 +1,40 @@
 package models
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
+)
+
+type UserRole int
+
+const (
+	UserRoleUser UserRole = iota
+	UserRoleAdmin
 )
 
 type User struct {
 	gorm.Model
-	Username     string `json:"username" gorm:"not null;unique"`
-	PublicSshKey string `json:"public_ssh_key" gorm:""`
+	Username     string   `json:"username" gorm:"not null;unique"`
+	Role         UserRole `json:"role" gorm:"index"`
+	PublicSshKey string   `json:"public_ssh_key" gorm:""`
+}
+
+func (s UserRole) String() string {
+	return []string{
+		"user",
+		"admin",
+	}[s]
+}
+
+func ParseUserRole(str string) (UserRole, error) {
+	for i, role := range []string{
+		"user",
+		"admin",
+	} {
+		if role == str {
+			return UserRole(i), nil
+		}
+	}
+	return UserRoleUser, fmt.Errorf("invalid user role: %s", str)
 }
