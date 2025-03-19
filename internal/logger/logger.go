448b32aa1c38059576deed6c49d89d9e00977bee
PhilemonBrain <philbrainy@gmail.com>
2025-03-19 21:12:55 +0100

    feat: custom seven logging levels

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -9,6 +9,8 @@ import (
 
 var talisLog = logrus.New()
 
+// Initialize sets up the logger with the appropriate configuration
+// and log level from environment variables
 func Initialize() {
 	var log = logrus.New()
 
@@ -36,4 +38,84 @@ func configureLogLevel() {
 	talisLog.SetLevel(level)
 	talisLog.Infof("Log level set to '%s'", level)
 
+}
+
+// GetLogger returns the configured logger instance
+func GetLogger() *logrus.Logger {
+	return talisLog
+}
+
+//Logrus has seven logging levels: Trace, Debug, Info, Warning, Error, Fatal and Panic.
+
+// Trace logs a message at the Trace level
+func Trace(args ...interface{}) {
+	talisLog.Trace(args...)
+}
+
+// Debug logs a message at the debug level
+func Debug(args ...interface{}) {
+	talisLog.Debug(args...)
+}
+
+// Info logs a message at the Info level
+func Info(args ...interface{}) {
+	talisLog.Info(args...)
+}
+
+// Warn logs a message at the Warn level
+func Warn(args ...interface{}) {
+	talisLog.Warn(args...)
+}
+
+// Error logs a message at the Error level
+func Error(args ...interface{}) {
+	talisLog.Error(args...)
+}
+
+// Fatal logs a message at the Fatal level
+func Fatal(args ...interface{}) {
+	talisLog.Fatal(args...)
+}
+
+// Panic logs a message at the Panic level
+func Panic(args ...interface{}) {
+	talisLog.Panic(args...)
+}
+
+// Formatted Logs
+//
+
+// Tracef logs a message at the TraceF level
+func Tracef(format string, args ...interface{}) {
+	talisLog.Tracef(format, args...)
+}
+
+// Debugf logs a message at the Debugf level
+func Debugf(format string, args ...interface{}) {
+	talisLog.Debugf(format, args...)
+}
+
+// Infof logs a message at the Infof level
+func Infof(format string, args ...interface{}) {
+	talisLog.Infof(format, args...)
+}
+
+// Warnf logs a message at the Warnf level
+func Warnf(format string, args ...interface{}) {
+	talisLog.Warnf(format, args...)
+}
+
+// Errorf logs a message at the Errorf level
+func Errorf(format string, args ...interface{}) {
+	talisLog.Errorf(format, args...)
+}
+
+// Fatalf logs a message at the Fatalf level
+func Fatalf(format string, args ...interface{}) {
+	talisLog.Fatalf(format, args...)
+}
+
+// Panicf logs a message at the Panicf level
+func Panicf(format string, args ...interface{}) {
+	talisLog.Panicf(format, args...)
 }
